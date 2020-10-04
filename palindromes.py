555897d612fdeb1ca626db37ea9ea89298506ce4
Nikita <71151110+TooManyEyes@users.noreply.github.com>
2020-10-05 00:00:07 +0300

    Реализована задача 2.3
    Простите, что чуть позже дедлайна - разбирался почему итертулс дают не то, что нужно

---
+++ palindromes.py
@@ -0,0 +1,15 @@
+from itertools import *
+
+
+def all_palindromes(alphabet, n):
+    ans = []
+    for word in combinations_with_replacement(alphabet+alphabet[::-1], n):
+        word = ''.join(word)
+        k = int(len(word) / 2)
+        b = word[::-1]
+        if word[0:k] == b[0:k]:
+            ans.append(''.join(word))
+    return '\n'.join(ans)
+
+
+print(all_palindromes('abcd', 4))
